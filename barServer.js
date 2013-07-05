04dfed88a59933e395ebd871effb2f246079d14d
Bill <sheltowt@gmail.com>
2013-07-05 16:43:16 -0700

    added data visualizations

---
+++ barServer.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+  app = express();
+
+app.get('/', function (req, res) {
+  res.sendfile('bar.html');
+});
+
+app.get('/data.csv', function (req, res) {
+  res.sendfile('data.csv');
+});
+
+app.get('/data.json', function (req, res) {
+  res.sendfile('data.json');
+});
+
+app.get('/newData.json', function (req, res) {
+  res.sendfile('newData.json');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Server running at' + port);
+});
